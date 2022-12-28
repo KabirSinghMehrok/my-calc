865fdc1d43cd56e65cffa010410266ccb9bbaead
Kabir Singh Mehrok <mehrok.kabir.singh@gmail.com>
2022-12-28 08:07:01 +0530

    Implement calculation related functions, minor changes in styling

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ class Calculator {
         // console.log(this.currentOperandElement + "not working");
         this.prevOperand = '';
         this.currentOperand = '0';
-        this.operator = '';
+        this.operator = undefined;
         this.updateDisplay();
     }
 
@@ -34,20 +34,59 @@ class Calculator {
     }
 
     chooseOperator(operator) {
+        if (this.operator != undefined && prevOperandElement != '') {
+            this.currentOperand = this.calculate();
+        }
+        this.operator = operator.toString();
+        this.prevOperand = this.currentOperand;
+        this.currentOperand = '0';
     }
 
     clear() {
         this.currentOperand = '0';
         this.prevOperand = '';
+        this.operator = undefined;
     }
 
     updateDisplay() {
         this.currentOperandElement.innerText = this.currentOperand;
-        this.prevOperandElement.innerText = this.prevOperand;
+        this.prevOperandElement.innerText = 
+        `${this.prevOperand} ${this.operator == undefined ? '' : this.operator}`;
+    }
+
+    equals() {
+        this.currentOperand = this.calculate().toString();
+        this.prevOperand = '';
+        this.operator = undefined;
     }
 
     calculate() {
+        let a = parseFloat(this.prevOperand);
+        let b = parseFloat(this.currentOperand);
+        let computed = undefined;
 
+        if (isNaN(a) || isNaN(b)) return computed;
+        switch(this.operator) {
+            case '÷':
+                computed = a / b;
+                break;
+            case '×':
+                computed = a * b;
+                break;
+            case '–':
+                computed = a - b;
+                break;
+            case '+':
+                computed = a + b;
+                break;
+            default:
+                return computed;
+        }
+
+        if (!Number.isInteger(computed)) {
+            computed = computed.toFixed(4);
+        }
+        return computed;
     }
 }
 
@@ -90,4 +129,9 @@ operatorButtons.forEach((button) => {
         calculator.chooseOperator(button.innerHTML);
         calculator.updateDisplay();
     })
+})
+
+equalsButton.addEventListener('click', () => {
+    calculator.equals();
+    calculator.updateDisplay();
 })
